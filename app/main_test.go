e95cc4441d3c017c1972687baa8231509323136d
shigaki daichi <d-shigaki@pro-seeds.co.jp>
2020-01-25 16:44:54 +0900

    refactoring

--- app/main_test.go
+++ app/main_test.go
@@ -1,9 +1,6 @@
 package main
 
 import (
-	"app/model"
-	"github.com/jinzhu/gorm"
-	_ "github.com/jinzhu/gorm/dialects/mysql"
 	"github.com/stretchr/testify/assert"
 	"net/http"
 	"net/http/httptest"
@@ -11,15 +8,6 @@ import (
 )
 
 func Test_Main(t *testing.T) {
-	db, err := gorm.Open("mysql", "user:password@tcp(db:3306)/test?charset=utf8mb4&parseTime=True&loc=Local")
-	if err != nil {
-		panic(err.Error())
-	}
-	
-	// make db scheme
-	db.AutoMigrate(&model.User{})
-	// insert test record
-	db.Create(&model.User{Id: 1, Name: "山田", Age: 11})
 
 	t.Run("Check HTTP 200", func(t *testing.T) {
 		w := httptest.NewRecorder()
@@ -31,7 +19,7 @@ func Test_Main(t *testing.T) {
 
 	t.Run("Check HTTP 404", func(t *testing.T) {
 		w := httptest.NewRecorder()
-		req, _ := http.NewRequest(http.MethodGet, "/user/2", nil)
+		req, _ := http.NewRequest(http.MethodGet, "/user/3", nil)
 		req.Header.Set("Content-Type", "application/json")
 		router.ServeHTTP(w, req)
 		assert.Equal(t, http.StatusNotFound, w.Code)
